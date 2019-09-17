01d12178e5b38efe3abe09cfe99ddc5a1832c772
inxudianz <william.inxcorp@gmail.com>
2019-09-17 09:12:17 +0700

    Initial Commit

---
+++ Movel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movel
+//
+//  Created by William Inx on 17/09/19.
+//  Copyright © 2019 William Inx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
